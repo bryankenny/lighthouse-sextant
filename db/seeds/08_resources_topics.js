440e8e388e5bce94a2561b938c356aca1f486b51
Lokasenna <jalovatt@hotmail.com>
2018-11-15 15:08:30 -0700

    Add seed data: Resources<->topics

--- db/seeds/08_resources_topics.js
+++ db/seeds/08_resources_topics.js
@@ -1,7 +1,38 @@
 exports.seed = function(knex, Promise) {
-  // return Promise.all([
-  //   knex('table_name').insert({id: 1, colName: 'rowValue1'}),
-  //   knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-  //   knex('table_name').insert({id: 3, colName: 'rowValue3'})
-  // ]);
+  return Promise.all([
+    knex('resources_topics').insert({resource_id: 1,  topic_id: 17}),
+    knex('resources_topics').insert({resource_id: 1,  topic_id: 9}),
+    knex('resources_topics').insert({resource_id: 1,  topic_id: 14}),
+    knex('resources_topics').insert({resource_id: 2,  topic_id: 14}),
+    knex('resources_topics').insert({resource_id: 3,  topic_id: 6}),
+    knex('resources_topics').insert({resource_id: 4,  topic_id: 5}),
+    knex('resources_topics').insert({resource_id: 5,  topic_id: 5}),
+    knex('resources_topics').insert({resource_id: 5,  topic_id: 8}),
+    knex('resources_topics').insert({resource_id: 6,  topic_id: 1}),
+    knex('resources_topics').insert({resource_id: 7,  topic_id: 9}),
+    knex('resources_topics').insert({resource_id: 7,  topic_id: 11}),
+    knex('resources_topics').insert({resource_id: 8,  topic_id: 9}),
+    knex('resources_topics').insert({resource_id: 8,  topic_id: 10}),
+    knex('resources_topics').insert({resource_id: 9,  topic_id: 2}),
+    knex('resources_topics').insert({resource_id: 9,  topic_id: 3}),
+    knex('resources_topics').insert({resource_id: 10, topic_id: 1}),
+    knex('resources_topics').insert({resource_id: 11, topic_id: 6}),
+    knex('resources_topics').insert({resource_id: 12, topic_id: 6}),
+    knex('resources_topics').insert({resource_id: 13, topic_id: 3}),
+    knex('resources_topics').insert({resource_id: 14, topic_id: 7}),
+    knex('resources_topics').insert({resource_id: 15, topic_id: 13}),
+    knex('resources_topics').insert({resource_id: 15, topic_id: 12}),
+    knex('resources_topics').insert({resource_id: 16, topic_id: 3}),
+    knex('resources_topics').insert({resource_id: 17, topic_id: 7}),
+    knex('resources_topics').insert({resource_id: 17, topic_id: 17}),
+    knex('resources_topics').insert({resource_id: 18, topic_id: 1}),
+    knex('resources_topics').insert({resource_id: 19, topic_id: 4}),
+    knex('resources_topics').insert({resource_id: 19, topic_id: 5}),
+    knex('resources_topics').insert({resource_id: 20, topic_id: 4}),
+    knex('resources_topics').insert({resource_id: 21, topic_id: 2}),
+    knex('resources_topics').insert({resource_id: 22, topic_id: 18}),
+    knex('resources_topics').insert({resource_id: 23, topic_id: 18}),
+    knex('resources_topics').insert({resource_id: 24, topic_id: 8}),
+    knex('resources_topics').insert({resource_id: 25, topic_id: 8})
+  ]);
 };
