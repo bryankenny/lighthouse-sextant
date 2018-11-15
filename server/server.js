6a8496e79667387ea396b5643d4ec4f3a7743204
cgehmlich <cgehmlich@gmail.com>
2018-11-15 20:48:34 +0000

    login, logout, register

--- server/server.js
+++ server/server.js
@@ -88,27 +88,18 @@ app.get("/profile", (req, res) => {
     res.redirect('/');
   }
 });
-app.get("/newResource", (req, res) => {
-  if (req.session.userID) {
-    res.render('newResource')
-  } else {
-    res.redirect('/');
-  }
-});
 app.get("/myResources", (req, res) => {
   if (req.session.userID) {
     let templateVars = {}
-    knex('resources').select().where({ownerID: userID})
-    .then(function(mine){
-      templateVars += mine
-    })
+    knex('resources').select().where({ ownerID: userID })
+      .then(function (mine) {
+        templateVars += mine
+      })
     knex('resources').join('reactions', 'resources.ownerID', 'reactions.userID')
-    .then(function(liked) {
-      templateVars =+ liked
-    })
-    
+      .then(function (liked) {
+        templateVars = + liked
+      })
     res.render('myResources', templateVars)
-
   } else {
     res.redirect('/');
   }
@@ -140,7 +131,8 @@ app.get("/index/:resourceID", (req, res) => {
 // POSTS -----------------------------------------------------------------------
 
 app.post('/register', (req, res) => {
-  if (!req.body.name) {
+  const name = req.body.name
+  if (!name) {
     const templateVars = {
       errCode: 400,
       errMsg: 'missing name'
@@ -149,46 +141,58 @@ app.post('/register', (req, res) => {
     res.render('error', templateVars);
   }
   else {
-    console.log(req.body.name);
-    knex('users').insert({ name: req.body.name })
-
-      .then(function () {
-        console.log(knex('users'));
-        res.render('index')
-      })
-      .catch(function (error) {
-        console.error(error);
-        res.render('error')
-      })
-
-    knex('users').select('id').where({ name: req.body.name })
+    knex('users').insert({ name: name }).returning(['id'])
       .then(function (result) {
-      req.session.userID = result;
+        req.session.userID = result[0].id;
+        console.log(result[0].id)
+        res.redirect('index')
+      }
+      )
+      .catch(function (error) {
+        let templateVars = {
+          errCode: 401,
+          errMsg: 'name already exists'
+        }
+        res.status(401);
+        res.render('error', templateVars);
       })
-    res.redirect('/');
   }
 });
 app.post('/login', (req, res) => {
-  knex('users').select('id').where({ name: req.body.name })
-  .then(function (result) {
-    req.session.userID = result;
-    res.render('index')
-  })
-  .catch(function (error) {
-    console.error(error);
-    res.render('error')
-  })
+  const name = req.body.name
+  knex('users').select('id').where({ name: name }).returning(['id'])
+    .then(function (result) {
+      req.session.userID = result[0].id;
+      console.log(result[0].id)
+      res.render('index')
+    })
+    .catch(function (error) {
+      let templateVars = {
+        errCode: 401,
+        errMsg: 'name not found'
+      }
+      res.status(401);
+      res.render('error', templateVars);
+    })
 });
 app.post('/logout', (req, res) => {
   req.session = null;
   res.redirect('/');
 });
 app.post('/index', (req, res) => {
-  knex('users').select('name').where({ id: req.session.userID }).then(function (result) {
-    knex('resources').insert({ url: req.body.url, title: req.body.title, description: req.body.description, topic: req.body.topic, owner: result }).then(function () {
-      console.log(knex('resources'))
-    });
-  })
+  const id = req.session.userID;
+  knex('users').select('name').where({ id: id })
+    .then(function (result) {
+      const url = req.body.url;
+      const title = req.body.title;
+      const description = req.body.description;
+      const topic = req.body.topic;
+
+      knex('resources').insert({ url: url, title: title, description: description, topic: topic, owner: result })
+        .then(function () {
+
+        });
+    })
 });
 
 app.post('/index/:resourceID', (req, res) => {
