0e7e4ceffcfc444e7269025f22d81a0f75c2c1b8
Lokasenna <jalovatt@hotmail.com>
2018-11-16 11:56:15 -0700

    Revert to earlier environment logic that was actually correct

--- server/server.js
+++ server/server.js
@@ -12,8 +12,23 @@ const app = express();
 const knexConfig = require("../knexfile");
 const knex = require("knex")(knexConfig[ENV]);
 
-const morgan = (ENV) && require('morgan');
-const knexLogger = (ENV) && require('knex-logger');
+if (ENV === "development") {
+
+  const morgan = require('morgan');
+  const knexLogger = require('knex-logger');
+
+  console.log("starting in dev environment");
+  // Load the logger first so all (static) HTTP requests are logged to STDOUT
+  // 'dev' = Concise output colored by response status for development use.
+  //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
+  app.use(morgan('dev'));
+
+  // Log knex SQL queries to STDOUT as well
+  app.use(knexLogger(knex));
+
+}
+
+
 
 const cookieSession = require("cookie-session");
 app.use(cookieSession({
@@ -25,13 +40,6 @@ app.use(cookieSession({
 // Seperated Routes for each Resource
 const usersRoutes = require("../routes/users");
 
-// Load the logger first so all (static) HTTP requests are logged to STDOUT
-// 'dev' = Concise output colored by response status for development use.
-//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-(morgan) && app.use(morgan('dev'));
-
-// Log knex SQL queries to STDOUT as well
-(knexLogger) && app.use(knexLogger(knex));
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
