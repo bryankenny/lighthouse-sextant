1c106fb98b0ccda43748f71012ac7c8705e206a6
Lokasenna <jalovatt@hotmail.com>
2018-11-17 16:31:37 -0700

    Remove node-sass dependencies; not used

--- server/server.js
+++ server/server.js
@@ -6,18 +6,15 @@ const ENV = process.env.ENV || "development";
 const PORT = process.env.PORT || 8080;
 
 const express = require("express");
+const cookieSession = require("cookie-session");
 const bodyParser = require("body-parser");
-const sass = require("node-sass-middleware");
 const app = express();
 
 const knexConfig = require("../knexfile");
 const knex = require("knex")(knexConfig[ENV]);
 
 const query = require("../db/queries")(knex);
-
 const routes = require("../routes/routes");
-
-
 
 if (ENV === "development") {
 
@@ -25,9 +22,12 @@ if (ENV === "development") {
   const knexLogger = require('knex-logger');
 
   console.log("starting in dev environment");
+
   // Load the logger first so all (static) HTTP requests are logged to STDOUT
   // 'dev' = Concise output colored by response status for development use.
-  //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
+  //         The :status token will be colored red for server error codes,
+  // yellow for client error codes, cyan for redirection codes, and uncolored
+  // for all other codes.
   app.use(morgan('dev'));
 
   // Log knex SQL queries to STDOUT as well
@@ -35,9 +35,6 @@ if (ENV === "development") {
 
 }
 
-
-
-const cookieSession = require("cookie-session");
 app.use(cookieSession({
   name: 'session',
   keys: ['secret', 'anothersecret', 'andanothersecret'],
@@ -46,15 +43,8 @@ app.use(cookieSession({
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/styles", sass({
-  src: __dirname + "/styles",
-  dest: __dirname + "/public/styles",
-  debug: true,
-  outputStyle: 'expanded'
-}));
 app.use(express.static("public"));
 
-// Mount all resource routes
 app.use("/", routes(knex, query));
 
 app.listen(PORT, () => {
