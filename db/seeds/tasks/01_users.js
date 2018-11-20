5428df64e84886c656ee0c7f9a172088a0147ad7
Lokasenna <jalovatt@hotmail.com>
2018-11-19 20:44:24 -0700

    Tidy up seed files

--- db/seeds/tasks/01_users.js
+++ db/seeds/tasks/01_users.js
@@ -1,7 +1,9 @@
 module.exports = function(knex, Promise) {
   return Promise.all([
-    knex("users").insert({name: "Adam", about: "White and nerdy"}),
-    knex("users").insert({name: "Bryan", about: "I'm Bryan, I dunno. I'm a cool guy."}),
-    knex("users").insert({name: "Cam", about: "Couldn't be reached for comment"})
+    knex("users").insert([
+      {name: "Adam", about: "White and nerdy"},
+      {name: "Bryan", about: "I'm Bryan, I dunno. I'm a cool guy."},
+      {name: "Cam", about: "Couldn't be reached for comment"}
+    ])
   ]);
 };
